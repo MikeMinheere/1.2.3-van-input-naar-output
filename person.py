c04a51ffec99d7f86ec11476c6a269fa57966ae6
Mike Minheere <99067548@mydavinci.nl>
2021-09-08 09:07:37 +0200

    person.py push

---
+++ person.py
@@ -0,0 +1,12 @@
+print("---------------------------------------")
+print("|")
+Naam = 'Mike Minheere'
+print('| Naam       : ' + Naam)
+Adres = 'Stationsstraat 9'
+print('| Adres      : ' + Adres)
+Postcode = '3371 AX'
+print('| Postcode   : ' + Postcode)
+Woonplaats = 'Hardinxveld-Giessendam'
+print('| Woonplaats : ' + Woonplaats)
+print("|")
+print("---------------------------------------")
